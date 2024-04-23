b995d2ff6d71f310cd7971f492f09afa85492d5d
Eugene Zveringa <zveringa@gmail.com>
2024-04-23 22:48:58 +0100

    23Apr

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 import { startTransition } from 'react';
 import './components/Field.jsx' ;
 import './components/Information.jsx' ;
@@ -7,7 +8,15 @@ import './components/GameLayout.jsx';
 import { Field } from './components/Field.jsx';
 
 export const Game = () => {
-	
+	const [currentPlayer, setCurrentPlayer] = useState('X');
+	const [isGameEnded, setIsGameEnded] = useState('false');
+	const [isDraw, setIsDraw] = useState('false');
+	const [field, setField] = useState([
+			'','','',
+			'','','',
+			'','','',
+		]);
+
 	return (
 		<div className="App">
 			<Field />
--- src/components/FieldLayout.jsx
+++ src/components/FieldLayout.jsx
@@ -5,6 +5,16 @@ export const FieldLayout = () => {
         <>
             <div className="container">
                 <div className="board">
+                <tbody> 
+              <tr>
+                {NUMS.slice(0, 3).map((number) => (
+                  <td key={number}>
+                     <Button number={number} onClick={handleCellClick} />
+                  </td>
+                ))}
+              </tr>
+              </tbody>
+              
                     <div className="row">
                         <div className="cell" data-cell></div>
                         <div className="cell" data-cell></div>
